98f1a50294a685a5c83036ecda8ed4d972188114
gorkemer <gorkemerr@gmail.com>
2020-04-20 19:02:32 +0300

    created memu.html and related folders

---
+++ database_config.php
@@ -0,0 +1,8 @@
+<?php
+  $servername = "localhost"; // local host or server ip address?
+  $port = 3306;
+  $username = "root";
+  $password = "karabiber10"; //if it asks mysql user root, if not: nyhe86bp
+  $dbname = "flankerDatabase";
+  $table = "dataColumns";
+?>
